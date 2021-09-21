bfe9838cce6735db5bf461051993ba3bbe0284e2
Tom Ames <tsames1@gmail.com>
2021-09-21 14:02:20 -0700

    added middleware to properly add the .png extension

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 2999;
 const app = express();
 const drinks = require('./models/drinks')
 
+for (drink of drinks) {
+    drink.image = drink.image.slice(0, -4) + ".png";
+}
+
 app.get('', (req, res) => {
     res.send('Welcome to the Github App!');
 })
@@ -12,6 +16,7 @@ app.get('/drinks', (req, res) => {
 })
 
 app.get('/drinks/:id', (req,res) => {
+    console.log(drinks[req.params.id].image)
     res.render('drinks_show.ejs', {drink : drinks[req.params.id]});
 })
 
