8fc182ed0591c882d721876adb09455d7e273071
tom <tsames1@gmail.com>
2021-09-21 16:44:19 -0700

    final commit

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require('express');
 const port = 2999;
 const app = express();
 const drinks = require('./models/drinks')
+const foods = require('./models/food')
 
 for (drink of drinks) {
     drink.image = drink.image.slice(0, -4) + ".png";
+}
+
+for (food of foods) {
+    food.image = food.image.slice(0, -4) + ".png";
 }
 
 app.get('', (req, res) => {
     res.send('Welcome to the Github App!');
 })
 
-app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs', {allDrinks : drinks});
+app.get('/menu', (req, res) => {
+    res.render('drinks_index.ejs', {menu: [drinks,foods]});
 })
 
 app.get('/drinks/:id', (req,res) => {
-    console.log(drinks[req.params.id].image)
     res.render('drinks_show.ejs', {drink : drinks[req.params.id]});
+})
+
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', { food: foods[req.params.id] });
 })
 
 app.listen(port, () => {
